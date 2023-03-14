1af6c45a24b7dc9d1a990f2b63c54f154c40a0e0
judenm <124482803+judenm@users.noreply.github.com>
2023-03-14 01:10:04 -0400

    Update Calculator.java

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 */
 @Service
 public class Calculator {
-        String umlNUMBER1 = "3";
+        String UMLNUMBER1 = "3";
         @Cacheable("sum")
         public int sum(int a, int b) {
                 return a + b;
